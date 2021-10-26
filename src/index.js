9cdd65180e9e4a0a105216cff3e332aaa80fa26b
Vagyasri <vagyasri2011@gmail.com>
2021-10-26 08:53:59 +0200

    Set path to sounds

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import './style.css';
+
+window.addEventListener('keydown', (e) => {
+  const audio = document.querySelector(`audio[data-key='${e.keyCode}']`);
+  if (!audio) return;
+  audio.play();
+});
